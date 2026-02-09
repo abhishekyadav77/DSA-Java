c3111663346280d5ec8079f7eeb8f583660cb26c
abhishekyadav77 <yadavvikky012@gmail.com>
2026-02-09 23:38:00 +0530

    Conditionals

---
+++ src/conditional2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class conditional2 {
+    public static void main(String[] args) {
+
+        // print numberrs from 1 to 5
+//        int i;
+//        for (i=1; i<=5; i+=2) {
+//            System.out.println(i);
+//        }
+
+        // print 1 to n
+//        Scanner no = new Scanner(System.in);
+//
+//        System.out.println("Enter any Integer");
+//        int n = no.nextInt();
+//
+//        for ( int num=1;num<=n; num++) {
+//            System.out.print(num +" ");
+//        }
+//
+        // While loop
+
+//        int num= 1;
+//        while(num<=5){
+//
+//            System.out.println(num);
+//            num+= 1;
+//        }
+        int n= 1;
+
+        do{
+            System.out.println(n);
+            n++;
+
+        }
+        while(n<=5);
+
+
+    }
+}
