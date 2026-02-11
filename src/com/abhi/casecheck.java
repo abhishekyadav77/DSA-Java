02663b57f2b271ed77174021ac142147d51b14e6
abhishekyadav77 <yadavvikky012@gmail.com>
2026-02-11 23:11:18 +0530

    Basics

--- src/com/abhi/casecheck.java
+++ src/com/abhi/casecheck.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class casecheck {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+       char ch = in.next().trim().charAt(0);
 
 
-        System.out.println(in.next().trim());
+        System.out.println(ch);
     }
 }
